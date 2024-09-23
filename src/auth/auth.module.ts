80e69c80ac41e682ff5e28546a5137531345872e
Tom <tom@Toms-MBP.lan>
2024-09-23 23:14:35 +0700

    integrate passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,20 +4,24 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from '@/modules/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './passport/local.strategy';
+import { JwtStrategy } from './passport/jwt.strategy';
 
 @Module({
-  imports: [UsersModule, JwtModule.registerAsync({
-    useFactory: async (configService: ConfigService) => ({
-      secret: configService.get<string>('JWT_SECRET'),
-      signOptions: {
-        expiresIn: configService.get<string>('JWT_ACCESS_TOKEN_EXPIRED'),
-      },
+  imports: [UsersModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_ACCESS_TOKEN_EXPIRED'),
+        },
+      }),
+      inject: [ConfigService],
     }),
-    inject: [ConfigService],
-  }),
-
+    PassportModule
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,17 +7,33 @@ import { JwtService } from '@nestjs/jwt';
 export class AuthService {
   constructor(private usersService: UsersService, private jwtService: JwtService) { }
 
-  async signIn(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<any> {
     global: true
     const user = await this.usersService.findByEmail(username);
     const isValidPassword = await comparePasswordHelper(pass, user?.password);
+    if (!user || !isValidPassword) return null;
+    return user;
+  }
 
-    if (!isValidPassword) {
-      throw new UnauthorizedException("username/password không hợp lệ");
-    }
-    const payload = { sub: user._id, username: user.email };
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id };
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: this.jwtService.sign(payload),
     };
   }
+
+  // Trước khi dùng thư viện
+  // async signIn(username: string, pass: string): Promise<any> {
+  //   global: true
+  //   const user = await this.usersService.findByEmail(username);
+  //   const isValidPassword = await comparePasswordHelper(pass, user?.password);
+
+  //   if (!isValidPassword) {
+  //     throw new UnauthorizedException("username/password không hợp lệ");
+  //   }
+  //   const payload = { sub: user._id, username: user.email };
+  //   return {
+  //     access_token: await this.jwtService.signAsync(payload),
+  //   };
+  // }
 }
